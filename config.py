5af606d9665b9e545137a6768e410f9f3e3da1f8
ankitchor <randhirsingh95669@gmail.com>
2026-08-06 12:31:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ── Git / updates ──────────────────────────────────────────────────────────
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ItsMeVishal0/VishalMusic.git")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ankitchor/Kirish")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
